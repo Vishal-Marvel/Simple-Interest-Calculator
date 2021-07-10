328f3b525166708e22e4a3baa2497b947904d0a8
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2021-07-10 18:55:44 +0530

    Final Upload for project

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if (principal > 0){
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        // interest += principal
+        var year = new Date().getFullYear()+parseInt(years);
+        var output = "If you deposit <span class=\"highlight\">"+ principal + "</span>,<br>"
+        output += "at an interest rate of <span class=\"highlight\">" + rate + "%</span>.<br>"
+        output += "You will receive an interest of <span class=\"highlight\">" + interest + "</span>,<br>"
+        output += "in the year <span class=\"highlight\">" + year + "</span>.<br>"
+        document.getElementById("result").innerHTML=output
+    }
+    else{
+        window.alert('Enter a positive number')
+        var principal_element = document.getElementById("principal")
+        principal_element.focus()
+        principal_element.value = ''
+    }
+    
     
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    rateval += '%'
+    document.getElementById("rate_val").innerText=rateval;
+}
